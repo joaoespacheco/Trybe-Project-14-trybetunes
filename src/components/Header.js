c4f13ac72645cdda6b5f0a4132cb020e32d6307e
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-07-01 21:32:49 -0300

    Conclusão do quarto requisito - Requisito 04/11

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
@@ -9,6 +10,8 @@ class Header extends React.Component {
 
     this.state = {
       user: '',
+      redirect: false,
+      localToRedirect: '',
     };
   }
 
@@ -16,21 +19,56 @@ class Header extends React.Component {
     const { onLoading } = this.props;
     const storageUser = await getUser();
     const { name } = storageUser;
-    this.setState(({ user: name }), () => onLoading());
+    this.setState({ user: name }, () => onLoading());
   }
 
+  redirectActive = ({ target }) => {
+    const { name } = target;
+    this.setState(({
+      redirect: true,
+      localToRedirect: name,
+    }));
+  };
+
   render() {
-    const { user } = this.state;
+    const { user, redirect, localToRedirect } = this.state;
     const { loadingStatus } = this.props;
 
     if (!loadingStatus) {
       return (
         <header data-testid="header-component">
           <h1 data-testid="header-user-name">{user}</h1>
+          <nav>
+            <button
+              data-testid="link-to-search"
+              name="search"
+              type="button"
+              onClick={ (event) => this.redirectActive(event) }
+            >
+              Search
+            </button>
+            <button
+              data-testid="link-to-favorites"
+              name="favorites"
+              type="button"
+              onClick={ (event) => this.redirectActive(event) }
+            >
+              Favorites
+            </button>
+            <button
+              data-testid="link-to-profile"
+              name="profile"
+              type="button"
+              onClick={ (event) => this.redirectActive(event) }
+            >
+              Profile
+            </button>
+          </nav>
+          { redirect ? <Redirect to={ `/${localToRedirect}` } /> : ''}
         </header>
       );
     }
-    return (<Loading />);
+    return <Loading />;
   }
 }
 
