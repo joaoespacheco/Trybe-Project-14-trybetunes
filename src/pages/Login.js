32b6f941114e3323cfa8b49f1acdc8de6c28fcd4
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-01-03 17:02:34 -0300

    Conclusão da estilização da página login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,34 +26,39 @@ class Login extends React.Component {
     const { redirectStatus } = this.state;
     if (!loadingStatus) {
       return (
-        <div data-testid="page-login">
-          <label htmlFor="login-input">
-            <p>Login:</p>
-            <input
-              name="loginInput"
-              value={ loginInput }
-              onChange={ onInputChange }
-              type="text"
-              placeholder="Digite seu login..."
-              data-testid="login-name-input"
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="login-submit-button"
-            disabled={ buttonStatus }
-            onClick={ () => this.loginOn() }
-          >
-            Entrar
-          </button>
-        </div>
+        <main data-testid="page-login" className="login-container">
+          <div className="login-container-content">
+            <h1>TrybeTunes</h1>
+            <div>
+              <label htmlFor="login-input">
+                <p>Login</p>
+                <input
+                  name="loginInput"
+                  value={ loginInput }
+                  onChange={ onInputChange }
+                  type="text"
+                  data-testid="login-name-input"
+                  placeholder="Digite seu login..."
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="login-submit-button"
+                disabled={ buttonStatus }
+                onClick={ () => this.loginOn() }
+              >
+                Play!
+              </button>
+            </div>
+          </div>
+        </main>
       );
     }
     return (
-      <>
+      <main>
         <Loading />
         { redirectStatus ? <Redirect to="/search" /> : ''}
-      </>
+      </main>
     );
   }
 }
